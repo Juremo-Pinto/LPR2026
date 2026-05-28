468a61ee5ca43d3bcc40a29fad1d89b904ec51e2
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-05-28 08:49:17 -0300

    More thins
    :p

---
+++ Aula4/C#/numInteiro/numInteiro.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+namespace Tabuada
+{
+    class Programinha
+    {
+        static void falar(string m) => Console.Write(m);
+        static void falarEnd(string m) => Console.WriteLine(m);
+        static int ouvir() => Convert.ToInt32(Console.ReadLine());
+        static double ouvir2() => Convert.ToDouble(Console.ReadLine());
+        static void Main()
+        {
+            while (true)
+            {
+                falar("Escreva um numero inteiro: ");
+                double numInt = ouvir2();
+                if (numInt % 1 == 0)
+                {
+                    if (numInt % 2 == 0)
+                    {
+                        falarEnd($"O numero {numInt} é par!");
+                    }
+                    else
+                    {
+                        falarEnd($"O numero {numInt} é impar!");
+                    }
+                }
+                else
+                {
+                    falarEnd("Eu disse inteiro...");
+                }
+            }
+
+        }
+
+    }
+
+}

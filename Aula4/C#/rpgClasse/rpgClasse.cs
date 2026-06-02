a2e71a04d63836c86bebcdb6ef15fd1a3baa5f72
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-06-02 14:13:21 -0300

    commit

---
+++ Aula4/C#/rpgClasse/rpgClasse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+namespace Tabuada
+{
+    class Programinha
+    {
+        static void falar(string m) => Console.Write(m);
+        static void falarEnd(string m) => Console.WriteLine(m);
+        static int ouvir() => Convert.ToInt32(Console.ReadLine());
+        static double ouvir2() => Convert.ToDouble(Console.ReadLine());
+        static void Main()
+        {
+/*
+Suponha que você está desenvolvendo um jogo
+de RPG. Você é responsável por implementar a
+mecânica de habilidades especiais para
+diferentes classes de personagens. Cada classe
+tem suas próprias habilidades especiais.
+Crie um programa que permita ao jogador
+escolher uma classe de personagem e, em
+seguida, exiba suas habilidades especiais
+correspondentes.
+*/
+            falar("escolha:");
+
+        }
+    }
+}

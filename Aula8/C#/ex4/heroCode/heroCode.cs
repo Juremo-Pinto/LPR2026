cf94c5a921b141617a9970cd4c610ca79cd75ee2
Juremo-Pinto <fredericoteodoroarantes@gmail.com>
2026-06-30 10:21:19 -0300

    Aula8
    i am become programer
    maker of codes

--- Aula8/C#/ex4/heroCode/heroCode.cs
+++ Aula8/C#/ex4/heroCode/heroCode.cs
@@ -2,15 +2,24 @@
 using System.Diagnostics.Metrics;
 using System.Net.NetworkInformation;
 using System.Security.Cryptography.X509Certificates;
+using System.Xml;
 namespace Tabuada
 {
     class Programinha
     {
+        static Hero[] hero = new Hero[5];
+        static Team[] team = new Team[3];
         public struct Hero
         {
             public string name;
             public string power;
             public int points;
+        }
+        public struct Team
+        {
+            public string namet;
+            public string powert;
+            public int pointst;
         }
         static void falar(string m) => Console.Write(m);
         static void falarEndl(string m) => Console.WriteLine(m);
@@ -19,29 +28,135 @@ namespace Tabuada
         static double ouvir2() => Convert.ToDouble(Console.ReadLine());
         static void heroRegistry()
         {
-            Hero[] team = new Hero[5];
 
-            for (int bananas = 0; bananas < 5; bananas++)
+            for (int bananas = 0; bananas < hero.Length; bananas++)
             {
-                falar($"Nome do heroi {bananas + 1}: ");
-                team[bananas].name = ouvir();
-                falar($"Poder do heroi {bananas + 1}");
-                team[bananas].power = ouvir();
-                falar($"Pontuação do heroi {bananas + 1}");
-                team[bananas].points = ouvirInt();
+                falar($"Nome do heroi {bananas}: ");
+                hero[bananas].name = ouvir();
+                falar($"Poder do heroi {bananas}");
+                hero[bananas].power = ouvir();
+                falar($"Pontuação do heroi {bananas}");
+                hero[bananas].points = ouvirInt();
 
             }
+
+            menu();
         }
         static void teamSelection()
         {
-            foreach (Hero hero in team)
+            falar($"Lista de herois para adicionar ao time: ");
+
+            for (int num = 0; num < hero.Length; num++)
             {
-                
+                falar($" Heroi {num + 1} => {hero[num].name} {hero[num].power} {hero[num].points}");
             }
+
+            for (int maracujas = 0; maracujas < team.Length; maracujas++)
+            {
+                int uh = ouvirInt();
+                if (0 <= uh && uh <= 4)
+                {
+                    team[maracujas].namet = hero[uh].name;
+                    team[maracujas].powert = hero[uh].power;
+                    team[maracujas].pointst = hero[uh].points;
+                }
+            }
+
+            menu();
         }
-        static void main()
+        static int teamPoints()
         {
-            heroRegistry();
+            int totalPoints = 0;
+
+            foreach (Team teamZ in team)
+            {
+                totalPoints += teamZ.pointst;
+            }
+
+            return totalPoints;
+
+        }
+
+        static void teamShow()
+        {
+
+            int batatas = 0;
+
+            falar("Equipe: ");
+            foreach (Team teamZ in team)
+            {
+                batatas++;
+                falar($"Integrante {batatas} =>");
+                falarEndl("");
+
+                falar($"{teamZ.namet}");
+                falar($"{teamZ.powert}");
+                falarEndl("");
+            }
+
+            int totalPoints = teamPoints();
+
+            falar($"Pontuação total => {totalPoints}");
+            falarEndl("");
+
+            menu();
+        }
+        static void preMenu()
+        {
+            falar("Bem vindo ao menu principal do criador de equipe!");
+            falarEndl("");
+            falar("Registre os heris disponiveis, seus poderes e pontuação.");
+            falar("1 para sim, 0 para sair");
+
+            int choice = ouvirInt();
+
+            if (choice == 1)
+            {
+                heroRegistry();
+            }
+            else
+            {
+                falar("Goodbye.");
+                Environment.Exit(0);
+            }
+
+        }
+        static void menu()
+        {
+            falarEndl("");
+            falarEndl("1 - Registrar heróis");
+            falarEndl("2 - Montar equipe");
+            falarEndl("3 - Mostrar equipe");
+            falarEndl("0 - Sair");
+
+            int select = ouvirInt();
+            switch (select)
+            {
+                case 1:
+                    heroRegistry();
+                    break;
+
+                case 2:
+                    teamSelection();
+                    break;
+
+                case 3:
+                    teamShow();
+                    break;
+
+                default:
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+        static void Main()
+        {
+            preMenu();
+
         }
     }
 }
+
+// i am become programer
+// maker of codes
--- Aula8/C#/ex5/requestCode/requestCode.cs
+++ Aula8/C#/ex5/requestCode/requestCode.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
+namespace Tabuada
+{
+    class Programinha
+    {
+        static Chamado[] chms = new Chamado[10];
+        public struct Chamado
+        {
+            public int num;
+            public string slc;
+            public string sec;
+            public int prty;
+            public string stats;
+            public string desc;
+        }
+        static void falar(string m) => Console.Write(m);
+        static void falarEndl(string m) => Console.WriteLine(m);
+        static string ouvir() => Convert.ToString(Console.ReadLine());
+        static int ouvirInt() => int.Parse(Console.ReadLine());
+        static double ouvir2() => Convert.ToDouble(Console.ReadLine());
+
+        static void cadastrarChamado()
+        {
+            
+        }
+        static void Main()
+        {
+
+        }
+    }
+}
